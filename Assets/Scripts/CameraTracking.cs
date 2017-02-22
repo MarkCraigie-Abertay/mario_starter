3fbca67f0fc517cbff3fa53cc769191a3eb92520
Mark <1500887@abertay.ac.uk>
2017-02-22 16:01:50 +0000

    Added tracking camera movement
    camera tracks player, except in Y coord since that's what mario does.

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTracking : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 offset;
+    private Vector3 camPos;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //fixed y coord, because mario does it too :P
+        camPos = new Vector3(player.transform.position.x + offset.x, transform.position.y, player.transform.position.z + offset.z);
+        transform.position = camPos;
+	}
+}
